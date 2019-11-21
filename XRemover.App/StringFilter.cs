6df74da4697913f2b3fe8e93d0bd0287406f0a52
YAVUZ KILIÇ <yavuz.kilic@koton.com.tr>
2019-11-21 22:45:12 +0300

    'StringFilter'

---
+++ XRemover.App/StringFilter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace XRemover.App
+{
+    public class StringFilter
+    {
+        private string string_;
+
+        public StringFilter(String string_)
+        {
+            this.string_ = string_;
+        }
+
+        public void removeXs()
+        {
+            if (StringHaveX() && StringHaveLengthGreaterThanOne())
+            {
+                this.string_ = this.string_[0]
+                        + this.string_.Replace("x", "").Replace("X", "")
+                        + (this.string_.Length - 1);
+            }
+        }
+
+        private bool StringHaveLengthGreaterThanOne()
+        {
+            return this.string_.Length > 1;
+        }
+
+        private bool StringHaveX()
+        {
+            return this.string_.Contains("x") || this.string_.Contains("X");
+        }
+
+        public String GetString()
+        {
+            return string_;
+        }
+    }
+}
---
+++ xRemover/StringFilterTest.cs
@@ -0,0 +1,34 @@
+﻿using XRemover.App;
+using Xunit;
+
+namespace xRemover
+{
+    public class StringFilterTest
+    {
+        [Fact]
+        public void ShouldRemoveXsOfStringMaintainingFirstAndLastX()
+        {
+            StringFilter firstString = new StringFilter("xxHxix");
+            firstString.removeXs();
+
+            StringFilter secondString = new StringFilter("abcd");
+            secondString.removeXs();
+
+            StringFilter thirdString = new StringFilter("xabcdx");
+            thirdString.removeXs();
+
+            StringFilter fourthString = new StringFilter("");
+            fourthString.removeXs();
+
+            StringFilter fiftyString = new StringFilter("x");
+            fiftyString.removeXs();
+
+            
+            Assert.Equal("abcd", secondString.GetString());
+            Assert.Equal("xabcdx", thirdString.GetString());
+            Assert.Equal("", fourthString.GetString());
+            Assert.Equal("x", fiftyString.GetString());
+            Assert.Equal("xHix", firstString.GetString());
+        }
+    }
+}
